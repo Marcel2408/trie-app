b4c3640e48294ce905523234b3e47917baec360c
Marcel Subirana <msubiranac@gmail.com>
2021-02-28 15:37:11 +0100

    feat(redux): setup redux provider, store and root-reducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.scss';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import App from './components/app/App';
 import BaseMap from './components/base-map/BaseMap';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <App>
-          <Route path="/" exact render={() => <Redirect to="/map" />} />
-          <Route path="/map" exact component={BaseMap} />
-        </App>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <App>
+            <Route path="/" exact render={() => <Redirect to="/map" />} />
+            <Route path="/map" exact component={BaseMap} />
+          </App>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

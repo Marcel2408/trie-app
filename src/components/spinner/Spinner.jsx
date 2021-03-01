5c1e95c0c2d64c03b9d8cc1be64eb519c985a992
Marcel Subirana <msubiranac@gmail.com>
2021-03-01 15:50:50 +0100

    perf(index.js): add lazy load with a spinner fallback

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => (
+  <div className="spinner">
+    <div className="rect1" />
+    <div className="rect2" />
+    <div className="rect3" />
+    <div className="rect4" />
+    <div className="rect5" />
+  </div>
+);
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.scss';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import App from './components/app/App';
-import BaseMap from './components/base-map/BaseMap';
 import store from './redux/store';
-import Checkout from './pages/checkout/Checkout';
+import Spinner from './components/spinner/Spinner';
+
+const BaseMap = lazy(() => import('./components/base-map/BaseMap'));
+const Checkout = lazy(() => import('./pages/checkout/Checkout'));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <App>
-            <Route path="/" exact render={() => <Redirect to="/map" />} />
-            <Route path="/map" exact component={BaseMap} />
-            <Route path="/checkout" exact component={Checkout} />
-          </App>
+          <Suspense fallback={<Spinner />}>
+            <App>
+              <Route path="/" exact render={() => <Redirect to="/map" />} />
+              <Route path="/map" exact component={BaseMap} />
+              <Route path="/checkout" exact component={Checkout} />
+            </App>
+          </Suspense>
         </Switch>
       </BrowserRouter>
     </Provider>
